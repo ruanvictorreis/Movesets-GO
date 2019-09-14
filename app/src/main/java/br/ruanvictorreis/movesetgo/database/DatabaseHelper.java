d48baad785bb187c49de18e2fb8c7f82dc5d014b
Ruan Reis <ruanvictorreis@gmail.com>
2019-09-14 14:31:35 -0300

    adding translations

--- app/src/main/java/br/ruanvictorreis/movesetgo/database/DatabaseHelper.java
+++ app/src/main/java/br/ruanvictorreis/movesetgo/database/DatabaseHelper.java
@@ -21,7 +21,7 @@ import br.ruanvictorreis.movesetgo.database.dao.TypeWeaknessDAO;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "MovesetPokemonGoPremium.db";
-    private static final int DATABASE_VERSION = 55;
+    private static final int DATABASE_VERSION = 56;
 
     private TypeDAO typeDAO;
 
