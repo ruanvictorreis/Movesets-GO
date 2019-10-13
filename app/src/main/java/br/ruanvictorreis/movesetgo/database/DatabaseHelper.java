6f1bee4396a8ea3715dc29e995263cb3fb3578db
ruanvictorreis <ruanvictorreis@gmail.com>
2019-10-12 21:46:22 -0300

    adding new pokemons

--- app/src/main/java/br/ruanvictorreis/movesetgo/database/DatabaseHelper.java
+++ app/src/main/java/br/ruanvictorreis/movesetgo/database/DatabaseHelper.java
@@ -21,7 +21,7 @@ import br.ruanvictorreis.movesetgo.database.dao.TypeWeaknessDAO;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "MovesetPokemonGoPremium.db";
-    private static final int DATABASE_VERSION = 59;
+    private static final int DATABASE_VERSION = 60;
 
     private TypeDAO typeDAO;
 
