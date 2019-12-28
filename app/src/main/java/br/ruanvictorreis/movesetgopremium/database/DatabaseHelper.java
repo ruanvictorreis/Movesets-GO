607245e7373cb0e362a0ef14b30b4d6b6360fb4e
ruanvictorreis <ruanvictorreis@gmail.com>
2019-12-28 18:03:54 -0300

    updating movesets

--- app/src/main/java/br/ruanvictorreis/movesetgopremium/database/DatabaseHelper.java
+++ app/src/main/java/br/ruanvictorreis/movesetgopremium/database/DatabaseHelper.java
@@ -21,7 +21,7 @@ import br.ruanvictorreis.movesetgopremium.database.dao.TypeWeaknessDAO;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "MovesetPokemonGoPremium.db";
-    private static final int DATABASE_VERSION = 65;
+    private static final int DATABASE_VERSION = 66;
 
     private TypeDAO typeDAO;
 
